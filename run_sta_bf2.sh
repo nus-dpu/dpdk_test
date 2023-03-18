d4c90068c15d38212089da13a541294aee0d94bd
qyn <12232014@zju.edu.cn>
2023-03-18 16:33:23 +0800

    run_sta_bf2.sh change

--- run_sta_bf2.sh
+++ run_sta_bf2.sh
@@ -3,7 +3,7 @@ for core_id in {18,18-19}
 do
     for flow_num in {100,10000}
     do
-        nohup expect remote_run_sta.expect ${test_time}> lab_results/log/remote.out 2>&1 &
+        nohup expect remote_run_sta.expect ${test_time} >> lab_results/log/remote.out 2>&1 &
         ./start_sta.sh pkt_send_mul_auto_sta enp216s0f0 "192.168.200.2" $core_id $flow_num 64 $test_time #149,bf2tocx4
         sleep(60)
     done
