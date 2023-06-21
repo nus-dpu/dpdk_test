be9350da25af637dd90078c69b2e8d8b9873ecd3
qynZJU <qyning710@zju.edu.cn>
2023-06-21 14:57:06 +0800

    add auto shell

---
+++ auto_run/testpmd_sample.sh
@@ -0,0 +1,2 @@
+sudo /usr/local/bin/dpdk-testpmd -l 0,2,4,6,8 -n 4 -a 3b:00.0 -- -i -a #cx5
+sudo /usr/local/bin/dpdk-testpmd -l 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 -n 4 -a 5e:00.0 -- -i -a #cx4
