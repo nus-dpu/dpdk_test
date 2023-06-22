e941df3c4bd5a8a8c8efdda57bdaa9c99942017f
qynZJU <qyning710@zju.edu.cn>
2023-06-22 16:13:45 +0800

    little change

--- lab_all/lab_1/run_sta_cx4.sh
+++ lab_all/lab_1/run_sta_cx4.sh
@@ -22,19 +22,12 @@ pkt_len=-1
 test_time_rcv=50
 test_time_send=30
 
-# flow_size_list=(1 2 5 10 100 1000 2000 5000 10000)
-# srcip_num_list=(10000 10000 1000 1000 1000 1000 1000 1000 1000)
-# dstip_num_list=(10000 5000 20000 10000 1000 100 50 20 10)
-
-flow_size_list=(1 2)
-srcip_num_list=(10000 10000)
-dstip_num_list=(10000 5000)
+flow_size_list=(1 2 5 10 100 1000 2000 5000 10000 100000)
+srcip_num_list=(10000 10000 1000 1000 1000 1000 1000 1000 1000 100)
+dstip_num_list=(10000 5000 20000 10000 1000 100 50 20 10 10)
 
 i=0
-for test in {0..1}
-# for core_id in {18,18-19,18-21,18-23,18-25,18-27,18-29,18-31,18-33,18-35}
-# for core_id in {"0-31","0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30","0,2,4,6,8,10,12,14","0,2,4,6","0,2"}
-# for core_id in {"0","1"}
+for test in {0..9}
 do
     flow_size=${flow_size_list[$test]}
     srcip_num=${srcip_num_list[$test]}
@@ -46,8 +39,8 @@ do
     send_run_para="flow_num $flow_num pkt_len $pkt_len flow_size $flow_size test_time $test_time_send srcip_num $srcip_num dstip_num $dstip_num"
     rcv_run_para="flow_num $flow_num pkt_len 64 flow_size $flow_size test_time $test_time_rcv srcip_num $srcip_num dstip_num $dstip_num"
 
-    echo nohup expect remote_run_sta_cx4.expect $run_path $user $password $remotefile $line $rcv_run_para >> ./lab_results/log/remote.out 2>&1 &
-    nohup expect remote_run_sta_cx4.expect $run_path $user $password $remotefile $line $rcv_run_para >> ./lab_results/log/remote.out 2>&1 &
+    echo "nohup expect remote_run_sta_cx4.expect $run_path $user $password $remotefile $line \"$rcv_run_para\" >> ./lab_results/log/remote.out 2>&1 &"
+    nohup expect remote_run_sta_cx4.expect $run_path $user $password $remotefile $line "$rcv_run_para" >> ./lab_results/log/remote.out 2>&1 &
     sleep 8s
 
     echo ./start_sta.sh $file $line 150 $core_id $run_path "$send_run_para"
@@ -59,7 +52,7 @@ do
     mkdir ./lab_results/${file}/send_$i
     mv ./lab_results/${file}/*.csv ./lab_results/${file}/send_$i/
     echo -e "off_thre\r\n${off_thre}" > ./lab_results/${file}/send_$i/para.csv
-        
+
     ssh qyn@10.15.198.149 "cd $run_path && mkdir ./lab_results/${remotefile}/rcv_$i"
     ssh qyn@10.15.198.149 "cd $run_path/lab_results/${remotefile}/ && mv *csv rcv_$i/"
        
