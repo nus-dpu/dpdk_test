6ee97fc0671184f3c90b0b43a8701baaa43947cd
qynZJU <qyning710@zju.edu.cn>
2023-07-13 21:15:48 +0800

    chaneg folder from flow_create to flow_rule

--- flow_create/flow_create_test2/README.md
+++
@@ -1,1 +0,0 @@
-add timer to calculate the install time of flow rules
--- lab_all/lab_3/run_sta_cx4.sh
+++ lab_all/lab_3/run_sta_cx4.sh
@@ -25,7 +25,8 @@ dstip_num=-1
 test_time_rcv=50
 test_time_send=30
 
-flow_num_list=(10 100 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+# flow_num_list=(10 100 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+flow_num_list=(10000 11000 12000 13000 14000 15000 16000 17000 18000 19000 20000)
 # flow_num_list=(10 20)
 cir_time=${#flow_num_list[@]}
 
