f3663adb09f7269130030a5eb3bf97ad529edb1c
qynZJU <qyning710@zju.edu.cn>
2023-07-24 19:28:55 +0800

    add flow_query

---
+++ flow_rule/flow_query/README.md
@@ -0,0 +1,11 @@
+flow_create_test2:
+    (changed from flow_create_test)
+    add timer to calculate the install time of flow rules
+
+flow_create_destroy:
+    (changed from flow_create_test2)
+    1.change to destroy flows
+
+flow_create_query:
+    (changed from flow_create_destroy)
+    1.change to query flows
---
+++ flow_rule/flow_query/multi_run_cx5.sh
@@ -0,0 +1,18 @@
+# run_path="/home/qyn/software/FastNIC"
+file_name="flow_destroy"
+
+if [[ ! -d "../lab_results/${file_name}" ]]
+then
+    mkdir ../lab_results/${file_name}
+fi
+
+rm -f ../lab_results/${file_name}/run_time*
+make
+
+for i in $(seq 1 2)
+do
+    rm -f ./run_time.csv
+    sudo ./build/${file_name} -l 31 -a 3b:00.0
+    mv ../lab_results/${file_name}/run_time.csv ../lab_results/${file_name}/run_time_${i}.csv
+    echo "finish test ${i}"
+done
