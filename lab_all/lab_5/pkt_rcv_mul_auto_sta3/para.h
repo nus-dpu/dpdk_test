95aced24ef16b2a8cb87add2067dadc7ec71b8c3
qyn <qyning710@zju.edu.cn>
2023-08-03 15:50:57 +0800

    add lab_5

---
+++ lab_all/lab_5/pkt_rcv_mul_auto_sta3/para.h
@@ -0,0 +1,2 @@
+#define MAX_RECORD_COUNT 10
+#define PROGRAM "pkt_rcv_mul_auto_sta3"
---
+++ lab_all/lab_5/pkt_send_mul_auto_sta5_2/para.h
@@ -0,0 +1,11 @@
+// #define SRC_IP_NUM 1000
+// #define DST_IP_NUM 1429
+// #define FLOW_NUM 100
+
+#define FLOW_NUM 100
+#define FLOW_SIZE 1000
+#define PKTS_NUM (FLOW_SIZE*FLOW_NUM)
+
+#define PKT_LEN 64
+#define MAX_RECORD_COUNT 60
+#define PROGRAM "pkt_send_mul_auto_sta5_2"
