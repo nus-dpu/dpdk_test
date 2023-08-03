dccfe366aba63605c40ab5f4d574c7700f9caf1a
qyn <qyning710@zju.edu.cn>
2023-08-03 15:53:18 +0800

    lab_5 update

---
+++ lab_all/lab_5/remote_bf2_config.sh
@@ -0,0 +1,7 @@
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "sed -i 's/#define OFFLOAD_THRE.*$/#define OFFLOAD_THRE ${ofld_pkts}/' /home/ubuntu/software/ovs_all/myovs/lib/fastnic_offload.h"
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "sudo ~/bin/ovs-ctl stop"
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "cd /home/ubuntu/software/ovs_all/myovs && make -j8"
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "cd /home/ubuntu/software/ovs_all/myovs && make install"
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "sudo ~/bin/ovs-ctl start --system-id=random"
+ssh -A -t qyn@10.15.198.160 ssh ubuntu@192.168.100.2 "/home/ubuntu/software/FastNIC/lab_all/lab_4/myovs_rule_install.sh"
+
