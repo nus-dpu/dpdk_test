77faef0d6170126cd092420e16db123ba9d525f0
qyn <qyning710@zju.edu.cn>
2023-09-12 21:20:24 +0800

    add 20.11 version lab6

---
+++ lab_all/lab_6/flow_create_test2_20.11/para.h
@@ -0,0 +1,4 @@
+#define FLOW_NUM 100000000
+#define GROUP_ID 2
+
+#define RUN_TIME_FILE "../lab_results/flow_create_test2_10.11/run_time.csv"
---
+++ lab_all/lab_6/flow_create_test3_20.11/para.h
@@ -0,0 +1,6 @@
+#define BASE_FLOW_NUM ((int)1e7)
+#define TEST_FLOW_NUM 100
+#define GROUP_ID 1
+
+#define RUN_TIME_FILE "../lab_results/flow_create_test3_10.11/run_time.csv"
+#define AVG_TIME_FILE "../lab_results/flow_create_test3_10.11/avg_time.csv"
