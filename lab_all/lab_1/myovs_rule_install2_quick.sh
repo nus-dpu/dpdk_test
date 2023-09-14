9e02e44964a0046708e394184ec60d07f79caa93
qynZJU <qyning710@zju.edu.cn>
2023-09-14 14:42:40 +0800

    fix myovs_rule_install2_quick.sh

--- lab_all/lab_1/myovs_rule_install2_quick.sh
+++ lab_all/lab_1/myovs_rule_install2_quick.sh
@@ -40,6 +40,7 @@ DST_I_CIRCLE_NUM=10000
 #src
 echo "start src add"
 for((i=0;i<$SRC_O_CIRCLE_NUM;i++));
+do
   sudo /home/ubuntu/software/ovs_all/ovs_install/usr/bin/ovs-ofctl add-flows ovsdpdk src_rule_$i.txt
   echo "finish add $(($i*$SRC_I_CIRCLE_NUM)) - $((($i+1)*$SRC_I_CIRCLE_NUM-1))"
 done
@@ -48,6 +49,7 @@ echo "finish src add"
 #dst
 echo "start dst add"
 for((i=0;i<$DST_O_CIRCLE_NUM;i++));
+do
   sudo /home/ubuntu/software/ovs_all/ovs_install/usr/bin/ovs-ofctl add-flows ovsdpdk dst_rule_$i.txt
   echo "finish add $(($i*$DST_I_CIRCLE_NUM)) - $((($i+1)*$DST_I_CIRCLE_NUM-1))"
 done
